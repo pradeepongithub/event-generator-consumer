68c20de1a7a9a0d12d60616d4977a5b3b87b74fd
Pradeep Yadav <pradeep.kumar@maersk.com>
2021-11-28 10:23:46 +0530

    refactor spaces inside init.sh

--- cql/init.sh
+++ cql/init.sh
@@ -3,8 +3,5 @@ set -e
 
 echo "cql scripts will run after cassandra starts..."
 
-
-
-
 cqlsh -f /cql/create_keyspaces.cql cassandra
 cqlsh -k event_generator_service_blue -f /cql/create_event_tables.cql cassandra
